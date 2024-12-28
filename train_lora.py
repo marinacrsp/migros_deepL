f48bf5941e5d4e46bc79416ab07071b99f4e646c
Marina <mcrespo@student-cluster.inf.ethz.ch>
2024-12-29 00:18:41 +0100

    eval Luca

--- train_lora.py
+++ train_lora.py
@@ -153,237 +153,13 @@ vae.requires_grad_(False)
 unet.requires_grad_(False)
 print()
 
-# def remove_param_from_optimizer(optim, param):
-#     for j in range(len(optim.param_groups)):
-#         optim_param_group_list = optim.param_groups[j]["params"]
-#         for i, optim_param in enumerate(optim_param_group_list):
-#             if param.shape == optim_param.shape and (param==optim_param).all():
-#                 del optim.param_groups[j]["params"][i]
-
 clear_cache()
-
-# """# SeLoRA class
-# Below Defines the SeLoRA injected linear layer class, shown in `Linear`.
-# """
-
-# class LoRALayer():
-#     """Function that introduces functionality to augment the neural network layers with LoRA parameters
-#     arguments:
-#     - r: rank
-#     - lora_alpha: scaling factor that adjusts the influence of lora matrixes on the weights
-#     - lora_dropout: dropout rate for lora specific parameter
-#     - merge_weights: determines whether Lora weights should be merged back into original matrixes Wo
-#     """
-#     def __init__(
-#         self,
-#         r: int,
-#         lora_alpha: int,
-#         lora_dropout: float,
-#         merge_weights: bool,
-#     ):
-#         self.r = r
-#         self.lora_alpha = lora_alpha
-#         # Optional dropout
-#         if lora_dropout > 0.:
-#             self.lora_dropout = nn.Dropout(p=lora_dropout)
-#         else:
-#             self.lora_dropout = lambda x: x
-#         # Mark the weight as unmerged
-#         self.merged = False
-#         self.merge_weights = merge_weights
-
-### This is the SeLoRA replacement of nn.Linear layer
-# class Linear(nn.Linear, LoRALayer):
-#     # LoRA implemented in a dense layer
-#     def __init__(
-#         self,
-#         in_features: int,
-#         out_features: int,
-#         r: int = 0,
-#         lora_alpha: int = 8,
-#         lora_dropout: float = 0.,
-#         EMA_factor: float = 0.6,
-#         # warmup_step_per_expand:int = 10,
-#         fan_in_fan_out: bool = False, # Set this to True if the layer to replace stores weight like (fan_in, fan_out)
-#         merge_weights: bool = True,
-#         **kwargs
-#     ):
-#         nn.Linear.__init__(self, in_features, out_features, **kwargs)
-#         LoRALayer.__init__(self, r=r, lora_alpha=lora_alpha, lora_dropout=lora_dropout,
-#                            merge_weights=merge_weights)
-
-#         self.r = r
-#         self.fan_in_fan_out = fan_in_fan_out
-
-#         # Actual trainable parameters
-#         if r > 0:
-#             self.lora_A = nn.Parameter(self.weight.new_zeros((r, in_features)))
-#             self.lora_B = nn.Parameter(self.weight.new_zeros((out_features, r)))
-#             self.lora_A_temp = nn.Parameter(self.weight.new_zeros((r + 1, in_features)))
-#             self.lora_B_temp = nn.Parameter(self.weight.new_zeros((out_features, r + 1)))
-#             self.use_temp_weight = False
-#             self.scaling = self.lora_alpha / self.r
-#             # Freezing the pre-trained weight matrix
-#             self.weight.requires_grad = False
-#             self.recorded_grad = 1
-
-#         self.reset_parameters()
-#         if fan_in_fan_out:
-#             self.weight.data = self.weight.data.T
-
-#     def get_active_rank(self):
-#         assert self.lora_A.shape[0] == self.lora_B.shape[1]
-#         return self.lora_A.shape[0]
-
-#     def reset_parameters(self):
-#         nn.Linear.reset_parameters(self)
-#         if hasattr(self, 'lora_A'):
-#             nn.init.zeros_(self.lora_B)
-#             nn.init.kaiming_uniform_(self.lora_A)
-#             nn.init.zeros_(self.lora_B_temp)
-#             nn.init.kaiming_uniform_(self.lora_A)
-
-
-#     def train(self, mode: bool = True):
-#         def T(w):
-#             return w.T if self.fan_in_fan_out else w
-#         nn.Linear.train(self, mode)
-#         if self.merge_weights and self.merged:
-#             # Make sure that the weights are not merged
-#             if self.r > 0:
-#                 self.weight.data -= T(self.lora_B @ self.lora_A) * self.scaling
-#             self.merged = False
-
-#     def eval(self):
-#         def T(w):
-#             return w.T if self.fan_in_fan_out else w
-#         nn.Linear.eval(self)
-#         if self.merge_weights and not self.merged:
-#             # Merge the weights and mark it
-#             if self.r > 0:
-
-#                 self.weight.data += T(self.lora_B @ self.lora_A) * self.scaling
-
-#             self.merged = True
-
-#     def forward(self, x: torch.Tensor,*args,  **kwargs):
-#         def T(w):
-#             return w.T if self.fan_in_fan_out else w
-
-#         if self.r > 0 and not self.merged:
-#             result = F.linear(x, T(self.weight), bias=self.bias)
-
-#             if self.r > 0:
-#                 if not self.use_temp_weight:
-#                     result += (self.lora_dropout(x) @ self.lora_A.T @ self.lora_B.T) * self.scaling
-#                 else:
-#                     self.lora_A_temp.data[:-1, :] = self.lora_A.data
-#                     self.lora_B_temp.data[:, :-1] = self.lora_B.data
-
-#                     result += (self.lora_dropout(x) @ self.lora_A_temp.T @ self.lora_B_temp.T) * self.scaling
-#             return result
-#         else:
-#             return F.linear(x, T(self.weight), bias=self.bias)
-
-#     def sum_square(self, _A, _B):
-#         return torch.sum(torch.square(_A)) + torch.sum(torch.square(_B))
-
-
-#     def get_rmse_grad(self):
-#         if not self.use_temp_weight:
-#             return self.sum_square(self.lora_A.grad, self.lora_B.grad)
-#         else:
-#             return self.sum_square(self.lora_A_temp.grad, self.lora_B_temp.grad)
-
-#     def change_to_temp(self):
-#         if not self.use_temp_weight:
-#             self.recorded_grad = self.get_rmse_grad()
-#             self.use_temp_weight = True
-#             self.lora_A.requires_grad = False
-#             self.lora_B.requires_grad = False
-#             self.lora_A_temp.requires_grad = True
-#             self.lora_B_temp.requires_grad = True
-#         else:
-#             self.use_temp_weight = False
-#             self.lora_A.requires_grad = True
-#             self.lora_B.requires_grad = True
-#             self.lora_A_temp.requires_grad = False
-#             self.lora_B_temp.requires_grad = False
-
-
-#     def get_ratio(self):
-#         if not self.use_temp_weight:
-#             return 0
-#         return self.get_rmse_grad() / self.recorded_grad
-
-
-
-#     def expand_rank(self, optimizer):
-
-#         old_lora_A = self.lora_A.data
-#         remove_param_from_optimizer(optimizer, self.lora_A)
-#         self.lora_A = nn.Parameter(self.weight.new_zeros((self.lora_A.shape[0] + 1, self.lora_A.shape[1])))
-#         nn.init.kaiming_uniform_(self.lora_A)
-#         self.lora_A.data[:-1, :] = old_lora_A
-
-
-#         old_lora_B = self.lora_B.data
-#         remove_param_from_optimizer(optimizer, self.lora_B)
-#         self.lora_B = nn.Parameter(self.weight.new_zeros((self.lora_B.shape[0], self.lora_B.shape[1] + 1)))
-#         nn.init.zeros_(self.lora_B)
-#         self.lora_B.data[:, :-1] = old_lora_B
-
-#         remove_param_from_optimizer(optimizer, self.lora_A_temp)
-#         remove_param_from_optimizer(optimizer, self.lora_B_temp)
-#         self.lora_A_temp = nn.Parameter(self.weight.new_zeros((self.lora_A.shape[0] + 1, self.lora_A.shape[1])))
-#         nn.init.kaiming_uniform_(self.lora_A_temp)
-#         self.lora_B_temp = nn.Parameter(self.weight.new_zeros((self.lora_B.shape[0], self.lora_B.shape[1] + 1)))
-#         nn.init.zeros_(self.lora_B_temp)
-
-#         optimizer.add_param_group({'params': self.lora_A})
-#         optimizer.add_param_group({'params': self.lora_B})
-#         optimizer.add_param_group({'params': self.lora_A_temp})
-#         optimizer.add_param_group({'params': self.lora_B_temp})
-
-#         return optimizer
-
 
 text_encoder = pipe.text_encoder
 text_encoder = text_encoder.requires_grad_(False)
 unet = pipe.unet
 unet = unet.requires_grad_(False)
 
-# def set_Linear_SeLoRA(model, target_modules):
-#     # works!
-#      # replace all linear layer (include q,k,v layer) into DyLoRA Layer.
-#     for name, layer in model.named_modules():
-#         if isinstance(layer, nn.Linear):
-
-#             LoRA_layer = Linear(
-#                   in_features = layer.in_features,
-#                   out_features = layer.out_features,
-#                   r = 1
-#             )
-
-#             LoRA_layer.weight = layer.weight
-#             LoRA_layer.weight.requires_grad = False
-#             LoRA_layer.bias = layer.bias
-#             if LoRA_layer.bias != None:
-#                 LoRA_layer.bias.requires_grad = False
-
-#             pointing_layer = model
-#             #if len(target_modules) == 0:
-#             if False:
-#                 if name.split('.')[-1] in target_modules:
-#                     for layer_name in name.split('.')[:-1]:
-#                         pointing_layer = getattr(pointing_layer, layer_name)
-#             else:
-#               if name.split('.')[-1] in target_modules:
-#                 for layer_name in name.split('.')[:-1]:
-#                         pointing_layer = getattr(pointing_layer, layer_name)
-
-#                 setattr(pointing_layer, name.split('.')[-1], LoRA_layer)
-#     return model
 
 unet_lora = set_Linear_SeLoRA(unet, UNET_TARGET_MODULES) # removed: set_rank
 text_encoder_lora = set_Linear_SeLoRA(text_encoder, TEXT_ENCODER_TARGET_MODULES) # removed: set rank
@@ -394,45 +170,6 @@ print_trainable_parameters(text_encoder_lora)
 print_trainable_parameters(unet_lora)
 
 
-# class ImageDataset(Dataset):
-#     def __init__(self, root_dir, df, tokenizer, size = 224, center_crop = True):
-#         self.root_dir = root_dir
-#         self.files = df['file_name'].tolist()
-#         self.findings = df['text'].tolist()
-#         self.tokenizer = tokenizer
-#         self.image_transforms = transforms.ToTensor()
-#         # transforms.Compose(
-#         #     [
-#         #         transforms.Resize(size, interpolation=transforms.InterpolationMode.BILINEAR),
-#         #         transforms.CenterCrop(size) if center_crop else transforms.RandomCrop(size),
-#         #         transforms.ToTensor(),
-#         #         transforms.Normalize([0.5], [0.5]),
-#         #     ]
-#         # )
-
-
-#     def __len__(self):
-#         return len(self.files)
-
-#     def __getitem__(self, idx):
-#         example = {}
-#         instance_image = Image.open(
-#             os.path.join(self.root_dir, self.files[idx])
-#         ).convert("RGB")
-
-#         example["instance_images"] = self.image_transforms(instance_image)
-#         example["instance_prompt_ids"] = self.tokenizer(
-#             self.findings[idx],
-#             truncation=True,
-#             padding="max_length",
-#             max_length=self.tokenizer.model_max_length,
-#             return_tensors="pt",
-#         ).input_ids
-
-#         return example
-
-
-
 metadata = pd.read_csv(reports_path)
 # Split the data into train, validation, and test sets
 train_df, temp_df = train_test_split(metadata, test_size=0.2, random_state=DEFAULT_RANDOM_SEED)
@@ -441,7 +178,7 @@ valid_df, test_df = train_test_split(temp_df, test_size=0.2, random_state=DEFAUL
 # ImageDataset initialization
 train_ds = ImageDataset(
     root_dir=Data_storage,
-    df=train_df[:3],
+    df=train_df,
     tokenizer=tokenizer,
 )
 
@@ -559,7 +296,7 @@ class Trainer:
             
             # clear_cache()
             torch.cuda.empty_cache()
-
+            del noisy_latents, latents, pixel_values, pormpt_idxs, model_pred, encoder_hidden_states
 
         ########################################################################
         ## add log and save model here TODO
@@ -643,8 +380,8 @@ class Trainer:
                 print(f'Training error at step {step}: {loss.item()}')
                 self.total_step += 1
 
-                clear_cache()
-
+                torch.cuda.empty_cache()
+                del noisy_latents, latents, pixel_values, pormpt_idxs, model_pred, encoder_hidden_states
 
                 if self.total_step % self.per_iter_valid == 0:
 
@@ -658,7 +395,7 @@ class Trainer:
                         check_and_make_folder(f'{save_result_path}/{folder_name}/trained_model')
                         check_and_make_folder(f'{save_result_path}/{folder_name}/trained_model/final_Unet')
                         check_and_make_folder(f'{save_result_path}/{folder_name}/trained_model/final_Text')
-                        print(f'Best validation error so far at step {step}: {valid_rmse}')
+                        print(f'Best validation error so far in epoch {epoch}, step {step}: {valid_rmse}')
                         self.unet.save_pretrained(f'{save_result_path}/{folder_name}/trained_model/final_Unet')
                         self.text_encoder.save_pretrained(f'{save_result_path}/{folder_name}/trained_model/final_Text')
 
@@ -683,7 +420,7 @@ trainer = Trainer(
     optimizer = optimizer,
     train_dl = train_loader,
     test_dl = valid_loader,
-    total_epoch = 2, #10,
+    total_epoch = 10, #10,
     WEIGHT_DTYPE = WEIGHT_DTYPE,
     threshould = 11.3,
     per_iter_valid = len(train_loader),
